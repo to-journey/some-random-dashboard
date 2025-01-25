2c1e29e6cfbe97718bb5d98827f6edb209f04c93
Evgenii Dolgopolov <edol@S-MacBook-Air.local>
2025-01-25 16:54:07 +0700

    deploy: updated mui provider

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react"
-import { AppProvider } from "@toolpad/core/nextjs"
+import { NextAppProvider } from "@toolpad/core/nextjs" // Replace with the new component
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter"
 import { DashboardLayout } from "@toolpad/core/DashboardLayout"
 import { NAVIGATION } from "@/lib/constants/sidebarNavigation"
@@ -9,15 +9,15 @@ import { sidebarTheme } from "@/lib/config/sidebarTheme"
 export default function DashboardPagesLayout({ children }): React.ReactNode {
   return (
     <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-      <AppProvider
+      <NextAppProvider // Use the new component here
         navigation={NAVIGATION}
         branding={{ title: "", logo: "" }}
         theme={sidebarTheme}
       >
         <DashboardLayout sx={{ overflow: "auto", width: "100%" }}>
-            {children}
+          {children}
         </DashboardLayout>
-      </AppProvider>
+      </NextAppProvider>
     </AppRouterCacheProvider>
   )
 }
